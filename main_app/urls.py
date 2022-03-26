2d038778a354ad7a5db97df2530854b85fae1561
Jihunp <edwardpark.park@gmail.com>
2022-03-26 15:07:44 -0700

    created search functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
-    path('dogs/', views.DogList.as_view(), name="dog-list"),
+    path('dogs/', views.DogList.as_view(), name="dog_list"),
+    path('dogs/new/', views.Dog_Create.as_view(), name="cat_create"),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
+from pipes import Template
 from re import template
+from unicodedata import name
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Dog
 
 # Create your views here.
 
@@ -20,24 +23,26 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name= "about.html"
 
-class Dog:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
+# class Dog:
+#     def __init__(self, name, img, age, gender):
+#         self.name = name
+#         self.img = img
+#         self.age = age
+#         self.gender = gender
 
 class DogList(TemplateView):
     template_name = 'doglist.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["dogs"] = dogs #add a key into the context object for view
+        name = self.request.GET.get("name")
+        if name != None:
+            context["dogs"] = Dog.objects.filter(name__icontains=name) #add a key into the context object for view
+            context["header"] = f"Searching for {name}"
+        else:
+            context["dogs"] = Dog.objects.all() #add a key into the context object for view
+            context["header"] = "Our Cats"
         return context
 
-dogs = [
-    Dog("woof", 5, "male"),
-    Dog("arf", 2, "female"),
-    Dog("power", 45, "female"),
-    Dog("drive", 423, "male"),
-]
+
 
